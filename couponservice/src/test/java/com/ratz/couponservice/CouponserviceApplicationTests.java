c3619a4cd7f32a4650ee6208a593d0fe66eacdf7
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-04-07 12:28:56 +0000

    first commit

---
+++ couponservice/src/test/java/com/ratz/couponservice/CouponserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratz.couponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
